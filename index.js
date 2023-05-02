d8b20d7fcc7ec3ab0de10463a621bce5725fcfca
Ronie083 <ahmedronie6@gmail.com>
2023-05-03 04:15:24 +0600

    chefs data created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Cooking Recipe is cooking')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.listen(port, () => {
+    console.log(`Chatgaiya hana API is running on port: ${port}`)
+})
